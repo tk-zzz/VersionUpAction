312564976fb782a0912169888fed3ef4940684ea
tk-zzz <49810315+tk-zzz@users.noreply.github.com>
2022-10-31 16:46:47 +0900

    version code update gradle task

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,3 +6,12 @@ plugins {
     id("com.android.library") version "8.0.0-alpha05" apply false
     id("org.jetbrains.kotlin.android") version "1.7.10" apply false
 }
+
+tasks.create("incrementVersionCode") {
+    val versionFile = file("./project.version")
+    val readText = versionFile.readText()
+    val currentVersionCode = readText.split("\n").find { it.contains("releaseVersionCode") }?.split(" ")?.get(2) ?: return@create
+    val incrementedVersionCode = currentVersionCode?.toInt()?.plus(1).toString()
+    val incrementedText = readText.replace(currentVersionCode, incrementedVersionCode)
+    versionFile.writeText(incrementedText, Charsets.UTF_8)
+}
